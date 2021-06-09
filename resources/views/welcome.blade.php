c5b864ccad21191d8b56d96ae0ac5ffd0a37b5e4
Renier Trenuela <rtrenuela@objectbright.com>
2021-06-09 11:33:01 +0800

    add images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title> {{ env('APP_NAME') }}</title>
+    <meta property="og:title" content="{{ env('APP_NAME') }}">
+    <meta property="og:description" content="{{ env('APP_NAME') }}">
+    <meta property="og:image" content="{{ asset('images/polo-logo-thumb.png') }}">
+    <meta property="og:url" content="{{ asset('images/polo-logo-thumb.png') }}">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
-    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/polo-logo-thumb.png') }}">
 
     <!-- CSS here -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
